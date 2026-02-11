7a2257c1aaab636f69bdb5153bb98cda9978aabd
Elvis <contact@diebel.ro>
2026-02-11 19:33:36 +0100

    Revert "Adaug pagina de landing DIEBEL PARFUM"
    This reverts commit ce39ae11427960dd1ff0e8e78b74cda6c18fd411.

--- src/App.js
+++ src/App.js
@@ -1,36 +1,23 @@
 import React from "react";
-import { BrowserRouter as Router, Routes, Route, useLocation } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Navbar from "./Navbar";
 import Footer from "./Footer";
-import Landing from "./Landing";
 import Home from "./Home";
 import MenPerfume from "./MenPerfume";
 import WomenPerfume from "./WomenPerfume";
 
-function AppContent() {
-  const location = useLocation();
-  const isLanding = location.pathname === "/";
-
+function App() {
   return (
-    <>
-      {!isLanding && <Navbar />}
+    <Router>
+      <Navbar />
       <main>
         <Routes>
-          <Route path="/" element={<Landing />} />
-          <Route path="/home" element={<Home />} />
+          <Route path="/" element={<Home />} />
           <Route path="/men" element={<MenPerfume />} />
           <Route path="/women" element={<WomenPerfume />} />
         </Routes>
       </main>
-      {!isLanding && <Footer />}
-    </>
-  );
-}
-
-function App() {
-  return (
-    <Router>
-      <AppContent />
+      <Footer />
     </Router>
   );
 }
